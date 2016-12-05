0cc4441e3bc5c4ecaba052257aed6dd8e65e55da
djposh <djposh@users.noreply.github.com>
2016-12-05 10:29:48 +0200

    new file:   CertificateExpirationCheck.ps1

---
+++ LoginScripts/CertificateExpirationCheck.ps1
@@ -0,0 +1,24 @@
+﻿# This script warns users about certirficates that will expire in
+# the next month. It is aimed at Windows 7 clients where the
+# New-CertificateNotificationTask CmdLet is not available.
+$DialogTitle = "Warning - Title"
+$DialogMessage = @"
+Warning
+Dialog Body
+"@
+
+$experationdates = (Get-Childitem Cert:\CurrentUser\my | select NotAfter).notafter
+$currentdate = get-date
+$warn = $false
+foreach ($edate in $experationdates){
+    if ($edate -lt $currentdate.AddMonths(1)){
+        if($edate -gt $currentdate){
+              $warn = $true  
+        }
+    }
+}
+
+if ($warn -eq $true){
+    $wshell = New-Object -ComObject Wscript.Shell
+    $wshell.Popup($dialogmessage, 0, $DialogTitle,0x0)
+}
